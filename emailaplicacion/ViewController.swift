d160b0b6366b1a1d26b4f16e5d1a7fceec968812
Edgar Esteban Hernández Maruri <maruri.hdez@outlook.com>
2019-01-18 23:13:09 -0600

    Initial Commit

---
+++ emailaplicacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emailaplicacion
+//
+//  Created by Edgar Esteban Hernández Maruri on 1/18/19.
+//  Copyright © 2019 Edgar Esteban Hernández Maruri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
