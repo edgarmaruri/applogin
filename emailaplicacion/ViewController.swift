4f7fceb0b30a65f14f3509d329c4c2e2003e6a15
Edgar Hernández <edgarmaruri@hotmail.com>
2019-01-19 22:49:06 -0600

    primer

--- emailaplicacion/ViewController.swift
+++ emailaplicacion/ViewController.swift
@@ -8,13 +8,104 @@
 
 import UIKit
 
+import FirebaseAuth
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sigInSelector: UISegmentedControl!
+    
+    
+    @IBOutlet weak var sigInLabel: UILabel!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var sigInButton: UIButton!
+    
+    
+    var isSignIn:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func sigInSelectorChanged(_ sender: UISegmentedControl) {
+    
+        isSignIn = !isSignIn
+    
+        if isSignIn{
+            sigInLabel.text = "Sig In"
+            sigInButton.setTitle("Sign In", for: .normal)
+        }
+        else{
+            sigInLabel.text = "Register"
+            sigInButton.setTitle("Register", for: .normal)
+        }
+        
+    }
+    
+    
+    
+    @IBAction func sigInButtonTapped(_ sender: UIButton) {
+    
+        //Hacer una validación en el mail y contraseña
+        
+        if let email = emailTextField.text, let pass = passwordTextField.text
+        {
+            //Revisar si esta entrando o registrandose
+            
+            if isSignIn{
+                //Ingreso del usuario con Firebase
+                Auth.auth().signIn(withEmail: email, password: pass, completion: { (user, error) in
+                    
+                    //Verificar el usuario nil
+                    
+                    if user != nil {
+                        //Usuario encontrado, ir pagina inicio
+                        
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else{
+                        //Error, verificar error y mostrar mensaje
+                        
+                        
+                    }
+                
+                })
+                
+                
+            }
+            else {
+                //Registrando el usuario con Firebase
+                
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    
+                    //verificzzr el usuario nil
+                    if user != nil {
+                        //Usuario encontrado, ir pantalla inicio
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else{
+                        //Error: verificar error y mostrar mensjae
+                    }
+                }
+            }
+        }
+        
+        
+        
+    
+    }
+    
+    
+    
 }
 
